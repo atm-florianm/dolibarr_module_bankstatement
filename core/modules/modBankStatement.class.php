2a4ab0cffe6d66458523f4d77da4c23ad2efc2cc
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2020-01-29 11:29:37 +0100

    TK1907-0506 - New: hooks class (copied from modulebuilder template)

--- core/modules/modBankStatement.class.php
+++ core/modules/modBankStatement.class.php
@@ -92,7 +92,7 @@ class modBankStatement extends DolibarrModules
 			'models'        => 0,                               // Set this to 1 if module has its own models directory (core/modules/xxx)
 			'css' => array('/bankstatement/css/bankstatement.css.php'),	// Set this to relative path of css file if module has its own css file
 			'js'  => array('/bankstatement/js/bankstatement.js.php'),   // Set this to relative path of js file if module must load a js on all pages
-			'hooks' => array(),                                 // Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context 'all'
+			'hooks' => array('bankcard'),                               // Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context 'all'
 			'moduleforexternal' => 0                            // Set this to 1 if feature of module are opened to external users
 		);
 
