b5ad5086347e9847efea58ca5174bbb87d8dba9a
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2020-02-03 10:25:22 +0100

    TK1907-0506 - FIX lib include missing when handling ajax queries

--- class/bankstatementformat.class.php
+++ class/bankstatementformat.class.php
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-
+dol_include_once('/bankstatement/lib/bankstatement.lib.php');
 /**
  * \file        class/bankstatementformat.class.php
  * \ingroup     bankstatement
