aea88234456d18c6a580272f10f235fb4dd747f9
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2020-02-14 11:24:00 +0100

    TK1907-0506 - fix rights definitions: concatenate rather than add module-specific rights number to module number

--- core/modules/modBankStatement.class.php
+++ core/modules/modBankStatement.class.php
@@ -152,12 +152,12 @@ class modBankStatement extends DolibarrModules
 		$this->rights = array();    // Permission array used by this module
 
 		$r=0;
-		$this->rights[$r][0] = $this->numero + $r;                 // Permission id (must not be already used);
+		$this->rights[$r][0] = $this->numero . $r;                 // Permission id (must not be already used);
 		$this->rights[$r][1] = 'ReadBankStatements';               // Permission label
 		$this->rights[$r][3] = 0;                                  // Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'read';                             // In php code, permission will be checked by test if ($user->rights->bankstatement->level1->level2)
 		$r++;
-		$this->rights[$r][0] = $this->numero + $r;                 // Permission id (must not be already used);
+		$this->rights[$r][0] = $this->numero . $r;                 // Permission id (must not be already used);
 		$this->rights[$r][1] = 'WriteBankStatements';              // Permission label
 		$this->rights[$r][3] = 0;                                  // Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'write';                            // In php code, permission will be checked by test if ($user->rights->bankstatement->level1->level2)
